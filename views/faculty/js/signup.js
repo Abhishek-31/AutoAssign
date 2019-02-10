77a92b8e376914d5702bfb05ce3bb72c9749e2ac
abhishek-31 <abhishek31ag@gmail.com>
2019-02-10 16:28:26 +0530

    Save

--- views/faculty/js/signup.js
+++ views/faculty/js/signup.js
@@ -1,9 +1,16 @@
-// $(document).ready(function() {
-//     console.log('File Working');
-// });
-
-var roll_no=$("#")
-function formValidation() {
-    console.log('Being called implicitly');
-    return true;
-}
+// // $(document).ready(function() {
+// //     console.log('File Working');
+// // });
+// $("#submit").on('click', function () {
+//     var name = $("#name");
+//     var dept=$("#dept");
+//     $.ajax({
+//         type: 'post',
+//         url: '/faculty/signup',
+//         async: false,
+//         data: { myFile: file }),
+//         contentType: "application/json",
+//         success: function (data) {
+//             alert(" Number of lines Read  :" + data[0].lines + "\n" + "Number of records saved:" + data[1].saved);
+//         }
+//     });});
